4ef72e8536547e2bbdab463cf041f036c4ef0f4d
YassineElkefi <yassine.elkefi6@gmail.com>
2025-10-16 21:14:28 +0100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Yassine EL KEFI on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
