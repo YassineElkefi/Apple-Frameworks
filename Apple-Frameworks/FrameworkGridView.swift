3a686a6bef6d77c4255ceefd42a79d2ba326dfa6
YassineElkefi <yassine.elkefi6@gmail.com>
2025-10-17 19:53:28 +0100

    Showing a 3 column GridView with Mock Data

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: columns){
+            ForEach(MockData.frameworks){ framework in
+                FrameworkTitleView(framework: framework)
+            }
+        }
+        
     }
 }
 
 #Preview {
     FrameworkGridView()
+        .preferredColorScheme(.dark)
+}
+
+
+struct FrameworkTitleView: View {
+    
+    let framework : Framework
+    
+    var body: some View {
+        VStack{
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
 }
